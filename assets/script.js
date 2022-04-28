28bf893ac4ef803156d8d5f43a69ee19e355b0a7
Jeremiah Brockett <98862284+Jeremiah-81@users.noreply.github.com>
2022-04-28 12:55:46 -0700

    updated all files

--- assets/script.js
+++ assets/script.js
@@ -1,117 +1,57 @@
-var hour = moment().format('dddd, MMMM Do YYYY, HH:mm:ss'); 
-var current = $('#currentDay')
+// changed time to military time for higher value.
+var hour = moment().format("MMMM Do YYYY, h:mm:ss a");
+var current = $("#currentDay");
 var time = "15:00:00";
-moment("15", "hh").format('LT') 
-$(document).ready(function(){
-current.text(hour);
+moment("15", "hh").format("LT");
+$(document).ready(function () {
+  current.text(hour);
 });
+
 $("currentDay").html(hour);
 
-
-
-
-
-
-
-
-// changed time to military time for higher value 
-
-moment("15", "hh").format('LT') 
-var formatted = moment(time, "HH:mm:ss").format("LT");
-
-
-
-
-
-
-
-
-
-
-
-// var container = document.getElementById("container")
-// var row = document.getElementsByClassName("gridRow");
- $(document).ready(function () {
-
-
-$(".saveBtn").on("click", function () {
+//this is where we save our items in locat storage for future use.
+$(document).ready(function () {
+  $(".saveBtn").on("click", function () {
     var note = $(this).siblings(".memo").val();
     var time = $(this).parent().attr("id");
-       
-    
-  //save note in locat storage for later use  
 
     localStorage.setItem(time, note);
-         })
+    localStorage.get(note);
+  });
 
-function timeSlot() {
-//change time to militery time to evaluate ther value.
-// change attr id and change moment too!!!!!
-  var currentTime = moment().format("h"); 
+  function timeSlot() {
+    // This keeps track of the timeline on whats happening in real time.
+    var currentTime = moment().format("hr");
     $(".time-slot").each(function () {
-  var timeSlot = parseInt($(this).attr("id").split("hour")[0]);
-  
-  // console.log(timeSlot, currentTime[0]);
+      var timeSlot = Number($(this).attr("id").substring(1));
+      console.log(timeSlot);
+      console.log(currentTime);
 
-            if (timeSlot <  parseInt (currentTime)) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (timeSlot === parseInt(currentTime)) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
+      if (timeSlot < parseInt(currentTime)) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (timeSlot === parseInt(currentTime)) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    });
+  }
 
-            }
-        })
-    }
+  $("#hr9 .description").val(localStorage.getItem("hr9"));
+  $("#hr10 .description").val(localStorage.getItem("hr10"));
+  $("#hr11 .description").val(localStorage.getItem("hr11"));
+  $("#hr12 .description").val(localStorage.getItem("hr12"));
+  $("#hr13 .description").val(localStorage.getItem("hr13"));
+  $("#hr14 .description").val(localStorage.getItem("hr14"));
+  $("#hr15 .description").val(localStorage.getItem("hr15"));
+  $("#hr16 .description").val(localStorage.getItem("hr16"));
+  $("#hr17 .description").val(localStorage.getItem("hr17"));
 
-
-    $("#h9 .description").val(localStorage.getItem("h9"));
-    $("#h10 .description").val(localStorage.getItem("h10"));
-    $("#h11 .description").val(localStorage.getItem("h11"));
-    $("#h12 .description").val(localStorage.getItem("h12"));
-    $("#h13 .description").val(localStorage.getItem("h13"));
-    $("#h14 .description").val(localStorage.getItem("h14"));
-    $("#h15 .description").val(localStorage.getItem("h15"));
-    $("#h16 .description").val(localStorage.getItem("h16"));
-    $("#h17 .description").val(localStorage.getItem("h17"));
-          
-    timeSlot();
-})
-
-
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!for reference only below line !!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-// console.log(formatted); 
-
-
-
-
-// Use Moment.js to format the following variables:
-// 1. What is today's date in the following format: Jan 1st, 1999?
-// var today = moment();
-// $("#1a").text(today.format("MMM Do, YYYY"));
- 
-
-// 3. Parse the following date, 11/3/2020, and convert it into the following format: Sunday, February 14th 2010, 3:25:50 pm.
-// var reformatDate = moment("11/3/20", "MM-DD-YY").format("dddd, MMMM Do YYYY, h:mm:ss a");
-// $("#3a").text(reformatDate);
-
-//$("#currentDay").html(current );
-
-
-// Do I need this section?
-// var minute = moment().format('LT');
-// var time = $('#timer')
-// $(document).ready(function(){
-// time.text(minute);
-// });
+  timeSlot();
+});
