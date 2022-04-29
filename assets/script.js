ad216bc4f7a31fb9830fe229157c971a17b65b1a
Jeremiah Brockett <98862284+Jeremiah-81@users.noreply.github.com>
2022-04-29 10:10:22 -0700

    updated script.js

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ var current = $("#currentDay");
 var time = "15:00:00";
 moment("15", "hh").format("LT");
 $(document).ready(function () {
-  console.log(localStorage.getItem("h15"))
+  console.log(localStorage.getItem("hr15"))
 
+  // This is for storing information in the console in real time.
   for (let i = 9; i<= 17; i++) {
     $(`#h${i}`).siblings(`.memo`).val(localStorage.getItem(`h${i}`));
     console.log($(`#h${i}`))
@@ -22,7 +23,7 @@ $(document).ready(function () {
     var note = $(this).siblings(".memo").val();
     var time = $(this).parent().attr("id");
 
-   console.log(localStorage.getItem("h15"))
+   console.log(localStorage.getItem("hr15"))
 
     localStorage.setItem(time, note);
     // localStorage.getItem(note);
@@ -54,15 +55,15 @@ $(document).ready(function () {
     });
   }
 
-  $("#hr9 .description").val(localStorage.getItem("hr9"));
-  $("#hr10 .description").val(localStorage.getItem("hr10"));
-  $("#hr11 .description").val(localStorage.getItem("hr11"));
-  $("#hr12 .description").val(localStorage.getItem("hr12"));
-  $("#hr13 .description").val(localStorage.getItem("hr13"));
-  $("#hr14 .description").val(localStorage.getItem("hr14"));
-  $("#hr15 .description").val(localStorage.getItem("hr15"));
-  $("#hr16 .description").val(localStorage.getItem("hr16"));
-  $("#hr17 .description").val(localStorage.getItem("hr17"));
+  $("#hr9 .memo").val(localStorage.getItem("hr9"));
+  $("#hr10 .memo").val(localStorage.getItem("hr10"));
+  $("#hr11 .memo").val(localStorage.getItem("hr11"));
+  $("#hr12 .memo").val(localStorage.getItem("hr12"));
+  $("#hr13 .memo").val(localStorage.getItem("hr13"));
+  $("#hr14 .memo").val(localStorage.getItem("hr14"));
+  $("#hr15 .memo").val(localStorage.getItem("hr15"));
+  $("#hr16 .memo").val(localStorage.getItem("hr16"));
+  $("#hr17 .memo").val(localStorage.getItem("hr17"));
 
   timeSlot();
 });
